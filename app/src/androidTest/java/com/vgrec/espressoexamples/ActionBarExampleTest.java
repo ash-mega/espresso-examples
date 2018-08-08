0861bc0c06cb7fcddcb51add35f2419d02b164f1
aw <aw@mega.co.nz>
2018-08-08 17:18:34 +1200

    delete uncorrect code

--- app/src/androidTest/java/com/vgrec/espressoexamples/ActionBarExampleTest.java
+++ app/src/androidTest/java/com/vgrec/espressoexamples/ActionBarExampleTest.java
@@ -1,9 +1,24 @@
+/*
+ * EspressoExamples
+ * com.vgrec.espressoexamples.espresso
+ *
+ * Created by Ash Wu on 8/08/18 4:50 PM.
+ * Copyright (c) 2018 mega.co.nz
+ */
 package com.vgrec.espressoexamples;
 
-import android.test.ActivityInstrumentationTestCase2;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
+import com.vgrec.espressoexamples.R;
 import com.vgrec.espressoexamples.activities.ActionBarExampleActivity;
 
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getContext;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -11,49 +26,45 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-/**
- * @author vgrec, created on 3/20/15.
- */
-public class ActionBarExampleTest extends ActivityInstrumentationTestCase2<ActionBarExampleActivity> {
-
-
-    public ActionBarExampleTest() {
-        super(ActionBarExampleActivity.class);
-    }
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        getActivity();
-    }
-
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class ActionBarExampleTest {
+    
+    @Rule
+    public ActivityTestRule<ActionBarExampleActivity> mActivityRule = new ActivityTestRule<>(ActionBarExampleActivity.class);
+    
+    @Test
     public void testClickOnMenuItem() {
         // Click on an item from ActionBar
         onView(withId(R.id.action_settings)).perform(click());
-
         // Verify the correct item was clicked by checking the content of the status TextView
         onView(withId(R.id.status)).check(matches(withText("Settings")));
+        
     }
-
+    
+    @Test
     public void testOverflowMenuOrOptionsMenu() {
+        sleep();
         // Open the action bar overflow or options menu (depending if the device has or not a hardware menu button.)
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getContext());
-
+        openActionBarOverflowOrOptionsMenu(getContext());
+        sleep();
         // Find the menu item with text "About" and click on it
         onView(withText("About")).perform(click());
-
+        sleep();
         // Verify the correct item was clicked by checking the content of the status TextView
         onView(withId(R.id.status)).check(matches(withText("About")));
     }
-
-    public void testActionMode() {
-        // Show the contextual ActionBar
-        onView(withId(R.id.toggle_action_mode)).perform(click());
-
-        // Click on a context item
-        onView(withId(R.id.action_one)).perform(click());
-
-        // Verify the correct item was clicked by checking the content of the status TextView
-        onView(withId(R.id.status)).check(matches(withText("ActionMode1")));
+    
+    private void sleep() {
+        sleep(1000);
+    }
+    
+    private void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
+
